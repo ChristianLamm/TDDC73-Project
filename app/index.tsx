93fcf6b56c56b1e143bd20a0845c5b80ba2e354b
ChristianLamm <christianlambert412@gmail.com>
2024-12-17 15:34:53 +0100

    fortsatt gnugga

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,29 @@
-import { Text, TextInput, View } from "react-native";
-import PWTest from "@/components/testPassword"
+import { Text, TextInput, View, StyleSheet, Button} from "react-native";
+import React, { useState } from 'react'
+//import PWTest from "@/components/testPassword"
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Index() {
+
+
+
+const [password, setValue] = useState('');
+
+
+
+const handleInputChange = (text: string) => {
+  setValue(text);
+  
+  
+   //alternativ 1: skicka vidare password till testPassword live för att avgöra styrka
+};
+
+const confirmButton = () => {
+
+  console.log('Password:', password)
+  //alternativ 2: skicka vidare password till testPassword genom att klicka Confirm för att avgöra styrka
+}
+
   return (
     <View
       style={{
@@ -10,10 +32,43 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Input your password.</Text>
-      <TextInput> . . .</TextInput>
-      <PWTest password="textInput" ></PWTest>
-      
+      <Text>Test the strength of your password</Text>
+      <TextInput
+       style={[styles.input, {color: '#000000'}]}
+       
+       value={password}
+       onChangeText={handleInputChange}
+       placeholder="Insert password..."
+       placeholderTextColor='#000000'
+       cursorColor='blue'
+       selectionColor='#000000'
+       
+       //color='#000000'
+      />
+      {/*<PWTest password="textInput" ></PWTest>*/}
+      <Button
+      title= 'Confirm'
+      onPress={confirmButton}
+      /> 
     </View>
   );
-}
+};
+
+  const styles = StyleSheet.create({
+
+    input: {
+
+      height: 40,
+      margin: 12,
+      padding: 20,
+      borderColor: '#000000',
+      borderWidth: 1,
+      color: '#000000',
+      //textDecorationColor: '#000000',
+    },
+    textInput:{
+
+    
+    }
+  })
+
